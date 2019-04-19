3e6c07035d302aa2ce236ce97d48a040db187343
github username <github email address>
2019-04-19 14:46:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+function justInvoke(callback) {
+  return callback()
+}
+
+function setThisWithCall(callback, object, argument) {
+  return callback.call(object, argument)
+}
+
+function setThisWithApply(callback, object, arguments) {
+  return callback.apply(object, [...arguments])
+}
+
+function returnNewFunctionOf(callback, object) {
+  return callback.bind(object)
+}
