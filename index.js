9e7bc45569191aec498eea94676f00ab8591c89a
grangerl330 <grangerl330@gmail.com>
2019-04-22 10:46:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(callback) {
+  return callback()
+}
+
+function setThisWithCall(callback, object, argument) {
+  return callback.call(object, argument)
+}
+
+function setThisWithApply(callback, object, argument) {
+  return callback.apply(object, argument)
+}
+
+function returnNewFunctionOf(callback, object) {
+  return callback.bind(object)
+}
